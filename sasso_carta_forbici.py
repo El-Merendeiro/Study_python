a4c4b6c88f3e0af7ad2c71b87ae297e4192b002c
ne088 <ne0880en@gmail.com>
2025-08-19 21:44:44 +0900

    sassocartaforbice

---
+++ sasso_carta_forbici.py
@@ -0,0 +1,22 @@
+print ("Benvenuto al classicco gioco, ")
+print ("SASSO, CARTA, FORBICI")
+
+knowledge = input ("Conosci gia le le regole?[Si/No]▷▶ ")
+
+if knowledge == "Si" or knowledge == "si":
+  print ("ok allora iniziamo subito")
+
+elif knowledge == "No" or knowledge == "no":
+  print ("ok nessun problema le regole sono semplicissime")
+  print ("entrambi scegliamo una delle 3 opzioni: Sasso, Carta o Forbici")
+  print ("La Carta batte il Sasso")
+  print ("Il Sasso batte le Forbici")
+  print ("Le Forbici battono la Carta")
+
+print ("scegli al mio tre")
+import time
+for t in range (3,0,-1):
+        print (t,end='\r', flush=True)
+        time.sleep(2)
+
+valore_player = input ("cosa scegli?▷▶ ")
