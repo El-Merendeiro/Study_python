0e92e85225ca14550f62c5de472fe658bd5750ba
ne088 <ne0880en@gmail.com>
2025-08-25 16:28:20 +0900

    organizzati nomifile e inizio dice_roller

---
+++ dice_roller_it_v0.py
@@ -0,0 +1,9 @@
+#questo sara' un dice roller per qualsiasi gioco da di ruolo o da tavola
+
+
+
+player_choice = input ("dado ")
+dice_face = int(player_choice [2])
+dice_number = int(player_choice [0])
+print (dice_number)
+print (dice_face)
