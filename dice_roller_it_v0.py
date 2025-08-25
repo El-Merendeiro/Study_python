744a35ca6fad2e263f32f68eb9f5158d598b6622
Merendeiro <merendeiro@kali>
2025-08-25 19:34:37 +0900

    roller upgrade 2

--- dice_roller_it_v0.py
+++ dice_roller_it_v0.py
@@ -1,12 +1,17 @@
 #questo sara' un dice roller per qualsiasi gioco da di ruolo o da tavola
 
-print ("\n\nBenvenuto in \033[1;41;37mRolly\033[0m\n")
-print ("Scegli il numero di dadi e le facce. Esempio: \033[47;30m3d6\033[0m tre dadi da sei facce")
-print ("Per le opzioni digita: \033[47;30mhelp\033[0m")
-print ("Per uscire digita: \033[47;30mexit\033[0m\n\n")
+print ("\n\nBenvenuto in \033[1;41;37mRolly\033[0m\n\n\
+Digita il numero di dadi e le facce. Esempio: \033[47;30m3d6\033[0m tre dadi da sei facce\n\
+Opzioni digita: \033[47;30mhelp\033[0m, \
+per uscire: \033[47;30mexit\033[0m\n\n")
 
-player_choice = input ("dado ")
+
+player_choice = input (">>---> ")
+while len(player_choice) < 3:
+  player_choice = input ("scegli ancora >>---> ")
+
 dice_face = int(player_choice [2])
 dice_number = int(player_choice [0])
+
 print (dice_number)
 print (dice_face)
