e538825932cf8f28910a02ed31b8030c6f91117d
Merendeiro <merendeiro@kali>
2025-08-26 17:40:34 +0900

    roller upgrade 3

--- dice_roller/dice_roller_it_v0.py
+++ dice_roller/dice_roller_it_v0.py
@@ -5,18 +5,20 @@ Digita il numero di dadi e le facce.\nEsempio: \033[47;30m3d6\033[0m tre dadi da
 Opzioni digita: \033[47;30mhelp\033[0m, \
 per uscire: \033[47;30mexit\033[0m\n\n")
 
+#variabili
+option = ["adv","dsv"]
+#input check e main split
+player_choice = input (">>---> ").split ()
+while len(player_choice[0]) < 3 or len(player_choice[1]) < 3 or player_choice[1] not in option:
+  player_choice = input ("scegli ancora >>---> ").split ()
 
-player_choice = input (">>---> ")
-while len(player_choice) < 3 and player_choice:
-  player_choice = input ("scegli ancora >>---> ")
+#dice_choice,dice_option,roll_option = player_choice.split()
+#dice_number = int(dice_choice [0])
+#dice_face = int(dice_choice [2])
 
-dice_choice,dice_option,roll_option = player_choice.split()
-dice_number = int(dice_choice [0])
-dice_face = int(dice_choice [2])
-
-print (dice_choice)
-print (dice_option)
-print (roll_option)
+#print (dice_choice)
+#print (dice_option)
+#print (roll_option)
 print (player_choice)
-print (dice_number)
-print (dice_face)
+#print (dice_number)
+#print (dice_face)
