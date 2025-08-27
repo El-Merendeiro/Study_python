ffcc26aab28f3897beb7d8ae8529c846609c4030
ne088 <ne0880en@gmail.com>
2025-08-28 05:59:43 +0900

    roller test

--- dice_roller/dice_roller_it_v0.py
+++ dice_roller/dice_roller_it_v0.py
@@ -7,7 +7,8 @@ per uscire: \033[47;30mexit\033[0m\n\n")
 
 #variabili
 
-option = ["adv","dsv"]
+list_dice_option = ["null","adv","dsv"]
+list_roll_option = ["null","ropt1","ropt2"]
 dice_choice = ""
 dice_option = ""
 roll_option = ""
@@ -19,8 +20,11 @@ def show_help ():
 seguito da 'd' e poi il numero di facce del dado")
 #input check e main split
 while True:
-  player_choice = input (">>---> ").split () #main split
-
+  player_choice = input (">>---> ").lower ().split () #main split
+  if player_choice[0] == "help":
+    show_help ()
+  elif player_choice[0] == "exit":
+    print ("sto uscendo")
 #check numero parole e aggiunta se mancano
   if len(player_choice) == 1:
     player_choice.append ("null")
@@ -28,19 +32,29 @@ while True:
   elif len(player_choice) == 2:
     player_choice.append ("null")
 
-#check massimo 3 parole e massimo 3 caratteri a parola
+#check massimo 3 parole e massimo 3 caratteri
+#check opzioni
   if len(player_choice) > 3\
 or len(player_choice[0]) < 3\
-or len(player_choice[1]) < 3\
-or len(player_choice[2]) < 3:
-    print ("non valido")
+or player_choice[1] not in list_dice_option\
+or player_choice[2] not in list_roll_option:
+    print ("non valido ricontrolla le opzioni")
+    continue
+
+#split player_choice check caratteri
+  dice_choice,dice_option,roll_option = player_choice
+  if "d" not in dice_choice:
+    print ("riprova con ex: 3d6")
+    continue
+  dice_choice = dice_choice.split ("d")
+  if not dice_choice[0].isdigit()\
+or not dice_choice[1].isdigit():
+    print ("non hai scirtto bene i dadi e le loro faccie ex: 2d6")
     continue
   else:
     break
 
 #split secondario per informazione dadi
-dice_choice,dice_option,roll_option = player_choice
-dice_choice = dice_choice.split ("d")
 dice_number = int(dice_choice [0])
 dice_face = int(dice_choice [1])
 
