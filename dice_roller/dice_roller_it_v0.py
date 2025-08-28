6a3cd5e92a80c70bf8c4070e595b82f2dce8db08
Merendeiro <merendeiro@kali>
2025-08-28 18:02:22 +0900

    input completato e rifinito

--- dice_roller/dice_roller_it_v0.py
+++ dice_roller/dice_roller_it_v0.py
@@ -5,23 +5,27 @@ Digita il numero di dadi e le facce.\nEsempio: \033[47;30m3d6\033[0m tre dadi da
 Opzioni digita: \033[47;30mhelp\033[0m, \
 per uscire: \033[47;30mexit\033[0m\n\n")
 
-#variabili
+#variabili principali
 list_dice_option = ["null","adv","dsv"]
 list_roll_option = ["null","ropt1","ropt2"]
 mode = ["help","exit","risiko"]
 dice_choice = ""
 dice_option = ""
 roll_option = ""
-dice_number = 0
-dice_face = 0
 
-#help
+
+#definizione della funzione show_help
 def show_help ():
-  print ("Digita il numero di dadi da lanciare,\
+  print ("Digita il numero di dadi da lanciare,\n\
 seguito da 'd' e poi il numero di facce del dado")
 
-#input check e main split
+
+#definizione della funzione di input, user_input. check dell'input, split delle opzioni, e split secondario del numeero di dadi/numero di facce
 def user_input ():
+#variabili secondarie
+  dice_number = 0
+  dice_face = 0
+#main input
   while True:
     player_choice = input (">>---> ").lower ().split () #main split
 #check numero parole e aggiunta se mancano
@@ -31,7 +35,7 @@ def user_input ():
     elif len(player_choice) == 2:
       player_choice.append ("null")
     elif len(player_choice) == 0:
-      print ("scrivi quslcosa")
+      print ("Digita \033[47;30mhelp\033[0m  per aiuto")
       continue
 #check massimo 3 parole e massimo 3 caratteri
 #check opzioni
@@ -39,29 +43,28 @@ def user_input ():
 or len(player_choice[0]) < 3\
 or player_choice[1] not in list_dice_option\
 or player_choice[2] not in list_roll_option:
-      print ("non valido ricontrolla le opzioni")
+      print ("Hai digitato qualcosa di sbagliato,\nricontrolla le opzioni nella guida digitando \033[47;30mhelp\033[0m.")
       continue
-
-#split player_choice check caratteri
+#check caratteri e opzioni, assegnazione varibili opzioni e split secondario di dice_choice
     dice_choice,dice_option,roll_option = player_choice
     if dice_choice in mode and dice_choice != "help":
       break
-    elif dice_choice == "help":
+    elif dice_choice == "help": #digitazione help
       show_help ()
       continue
     if "d" not in dice_choice:
-      print ("riprova con ex: 3d6")
+      print (f"\033[47;30m{dice_choice}\033[0m non e' una scelta valida,\nesempio: per lanciare 2 dadi da 6 facce digita \033[47;30m2d6\033[0m.\nPer la guida digita \033[47;30mhelp\033[0m.")
       continue
-    dice_choice = dice_choice.split ("d")
+    dice_choice = dice_choice.split ("d") #split secondario
     if not dice_choice[0].isdigit()\
 or not dice_choice[1].isdigit():
-      print ("non hai scirtto bene i dadi e le loro faccie ex: 2d6")
+      print (f"Hai digitato una scelta non valida,\nesempi validi: 2d6, 3d8, 1d100.\nPer la guida digita \033[47;30mhelp\033[0m ")
       continue
     else:
       dice_number = int(dice_choice [0])
       dice_face = int(dice_choice [1])
       break
-      return dice_choice, dice_option, roll_option, player_choice, dice_number, dice_face
+  return dice_choice, dice_option, roll_option, player_choice, dice_number, dice_face
 dice_choice, dice_option, roll_option, player_choice, dice_number, dice_face = user_input()
 #fine input
 
@@ -72,4 +75,3 @@ print (player_choice)
 if dice_number !=0:
   print (dice_number)
   print (dice_face)
-show_help ()
