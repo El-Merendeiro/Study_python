03a6a2ceb316ee1f70f95b39089ded94b1f08a9a
ne088 <ne0880en@gmail.com>
2025-08-29 17:16:13 +0900

    test

--- dice_roller/test2.py
+++ dice_roller/test2.py
@@ -7,7 +7,12 @@ for t in range (1,y+1):
   possibili += [t]
 
 result = [random.choice (possibili) for _ in range (x)]
-
 print (possibili)
 print (result)
+massimo = result[0]
+for n in result:
+  print (n)
+  if n > massimo:
+    massimo = n
+print (massimo)
 
