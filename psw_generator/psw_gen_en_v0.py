2075d37222ce922e4362b678e63abf5ae51f0aaf
ne088 <ne0880en@gmail.com>
2025-09-12 16:34:17 +0900

    inizio funzioni input

--- psw_generator/psw_gen_en_v0.py
+++ psw_generator/psw_gen_en_v0.py
@@ -16,8 +16,41 @@ def show_help ():
 
 #input
 def mode_input ():
-  mode_select = input ("Choose your password genration mode: Auto/User -> ").lower ()
-  return mode_select
+  while True:
+    mode_select = input ("Choose your password genration mode: Auto/User -> ").lower ()
+    if mode_select == "auto" or mode_select == "user":
+      return mode_select
+    print ("invalid input.")
+    continue
+
+def auto_input ():
+  level = ["weak","normal","strong","very strong"]
+  while True:
+    level_select = input ("Chose password's security level:\n"
+"weack, normal, strong, very strong -> ").lower ()
+    if level_select in level:
+      return level_select
+    print ("invalid input")
+    continue
+
+def user_input ():
+  answer = ["y","yes","n","no"]
+  while True:
+    lenght_select = input ("How many carachters your password should be? type a number -> ")
+    if not lenght_select.isdigit() and lenght_select < 50:
+      print ("type a number smaller than 50")
+      continue
+    carachter_type = input ("Do you want include letter in your password? y/n -> ").lower()
+    if not carather_type in answer:
+      print ("invalid input")
+      continue
+    if carachter_type == "yes" or carachter_type == "y":
+      carachter_type = True
+    elif carachter_type == "no" or carachter_type == "n":
+      carachter_type = False
+
 
 mode_select = mode_input ()
+level_select = auto_input ()
 print (mode_select)
+print (level_select)
