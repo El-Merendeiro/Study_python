e60bde0ea1a0163e726f47df9cf6bb0cb0191637
ne088 <ne0880en@gmail.com>
2025-09-13 18:16:15 +0900

    miglioramento input

--- psw_generator/psw_gen_en_v0.py
+++ psw_generator/psw_gen_en_v0.py
@@ -19,6 +19,9 @@ def show_help ():
 def mode_input ():
   while True:
     mode_select = input ("Choose your password genration mode: Auto/User -> ").lower ()
+    if mode_select == "help":
+      show_help ()
+      continue
     if mode_select == "auto" or mode_select == "user":
       return mode_select
     print ("invalid input.")
@@ -29,15 +32,30 @@ def auto_input ():
   level = ["weak","normal","strong","very strong"]
   while True:
     level_select = input ("Chose password's security level:\n"
-"weack, normal, strong, very strong -> ").lower ()
+"weak, normal, strong, very strong -> ").lower ()
     if level_select in level:
       return level_select
     print ("invalid input")
     continue
 
+
+
 #user input option
+
+
+def ask_y_n (question):
+  while True:
+    user_select = input (question).lower ()
+    if user_select == "yes" or user_select == "y":
+      return True
+    elif user_select =="no" or user_select == "n":
+      return False
+    else:
+      print ("invalid input")
+      continue
+
+
 def user_input ():
-  answer = ["y","yes","n","no"]
   while True:
     lenght_select = input ("How many carachters your password should be? type a number -> ")
     if not lenght_select.isdigit():
@@ -45,35 +63,24 @@ def user_input ():
       continue
     lenght_select = int(lenght_select)
     if lenght_select > 50:
-      print ("type a number smoller than 50")
+      print ("type a number smaller than 50")
       continue
-    carachter_selct = input ("Do you want include letter in your password? y/n -> ").lower()
-    if not carather_select in answer:
-      print ("invalid input")
-      continue
-    if carachter_select == "yes" or carachter_select == "y":
-      carachter_select = True
-    elif carachter_select == "no" or carachter_select == "n":
-      carachter_select = False
-    special_select = input ("Do you want include special caracter? y/n -> ").lower()
-    if not special_select in answer:
-      print ("invalid input")
-      continue
-    if special_select =="yes" or special_select == "y":
-      special_select = True
-    elif special_select == "no" or specia_select == "n":
-      special_select = False
-    capital_select = input ("Do you want include capital caracter? y/n/ -> ").lower()
-    if not capital_select in answer:
-      print ("invalid input")
-      continue
-    if capital_select =="yes" or capital_select == "y":
-      capital_select = True
-    elif capital_select == "no" or capital_select == "n":
-      capital_select = False
+    return lenght_select
 
 
-mode_select = mode_input ()
-level_select = auto_input ()
+
+
+
+
+mode_select = mode_input()
+if mode_select == "auto":
+  level_select = auto_input ()
+elif mode_select == "user":
+  lenght_select = user_input ()
+
+  characters_select = ask_y_n ("Do you want include letters in your password? y/n ")
+  capital_select = ask_y_n ("Do you want include CAPITAL characters in your password?")
+  special_select = ask_y_n ("Do you want include special characters inyour password? y/n")
+
+
 print (mode_select)
-print (level_select)
